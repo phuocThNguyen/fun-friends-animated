4bb817516feac7213971cff69ae747225df46195
PhuocNguyem <phuocthien1996@gmail.com>
2021-08-25 15:50:51 +1000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,7 @@ yarn run build
 cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:phuocThNguyen/fun-friends-activity-book-animated.git gh-pages
+git push -f git@github.com:phuocThNguyen/fun-friends-animated.git master:gh-pages
 cd -
