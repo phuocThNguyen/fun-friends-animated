0ad5230030b27e487495acda040643690254c5cb
PhuocNguyem <phuocthien1996@gmail.com>
2021-09-16 15:38:58 +1000

    add pwa

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from "./store";
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.directive('visible', function(el, binding) {
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,28 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/fun-friends-animated/'
-        : '/'
+        : '/',
+    pwa: {
+        name: 'Fun Friends Skills',
+        themeColor: '#00ce7c',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        manifestOptions: {
+            name: 'Fun Friends Skills',
+            short_name: 'Fun Friends Skills',
+            start_url: '.',
+            display: 'standalone',
+            theme_color: '#00ce7c'
+        },
+        iconPaths: {
+            favicon32: 'img/logos/favicon-32x32.png',
+            favicon16: 'img/logos/favicon-16x16.png',
+            appleTouchIcon: 'img/logos/apple-touch-icon-152x152.png',
+            msTileImage: 'img/logos/msapplication-icon-144x144.png'
+        },
+
+        // configure the workbox plugin
+        workboxPluginMode: 'InjectManifest',
+    }
 }
