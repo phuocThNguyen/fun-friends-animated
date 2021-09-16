dbf844730626b521d83134b3edca53856195e37e
PhuocNguyem <phuocthien1996@gmail.com>
2021-09-16 15:42:49 +1000

    add service-worker.js in src folder

--- vue.config.js
+++ vue.config.js
@@ -24,5 +24,10 @@ module.exports = {
 
         // configure the workbox plugin
         workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            swSrc: 'src/service-worker.js',
+            // ...other Workbox options...
+        }
     }
 }
