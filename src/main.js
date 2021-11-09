ed29e1cfe9dd2cba4f154a5c15097135fc4878b7
Phuoc Nguyen <phuocthien1996@gmail.com>
2021-11-09 16:50:24 +1000

    interactive for s1p20 21

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import store from "./store";
 // import './registerServiceWorker'
 import wb from './registerServiceWorker';
+// import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 Vue.prototype.$workbox = wb
 Vue.config.productionTip = false
 Vue.directive('visible', function(el, binding) {
   el.style.visibility = binding.value ? 'visible' : 'hidden';
 });
+// Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 
 new Vue({
   store,
