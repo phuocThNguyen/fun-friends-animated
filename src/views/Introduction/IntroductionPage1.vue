19400e49220b62661e643aec266839dcd97542d0
Phuoc Nguyen <phuocthien1996@gmail.com>
2022-02-24 11:41:31 +1000

    minor fix

--- src/views/Introduction/IntroductionPage1.vue
+++ src/views/Introduction/IntroductionPage1.vue
@@ -1,21 +1,13 @@
 <template>
   <div class="interactive-container">
     <img src="../../assets/images/introduction/page1.jpg" alt="page-1" class="session-background">
-    <audio src="../../assets/sounds/introduction/1Animated_Book_Title.mp3" ref="voice"/>
+    <audio src="../../assets/sounds/introduction/1Animated_Book_Title.mp3" autoplay ref="voice"/>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IntroductionPage1',
-  methods: {
-    playVoiceOver() {
-      setTimeout(() => {this.$refs.voice.play()}, 500)
-    }
-  },
-  mounted() {
-    this.playVoiceOver();
-  }
 }
 </script>
 
